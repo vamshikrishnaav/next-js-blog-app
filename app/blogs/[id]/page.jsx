02ce1db6d1bed96d626e28302b8a3dce958f6a91
Vamshi Krishna <vamshi973133@gmail.com>
2026-01-07 14:48:38 +0530

    commit

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -20,17 +20,25 @@ const ClientPage = ({ params }) => {
     return <div>Blog not found</div>
   }
 
-  return (
+  return ( data?<>
     <div className=' bg-gray-200 px-5 py-5 md:px-12 lg:px-28'>
       <div className=' flex justify-between items-center'>
         <Image src={assets.logo} alt='' width={180} className='w-32.5 sm:w-auto'/>
         <button className=' flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
           Get started <Image src={assets.arrow} alt='' />
           </button>
-
       </div>
-     
+      <div className=' text-center my-24'>
+          <h1 className=' text-2xl sm:text-5xl font-semibold max-w-175 mx-auto'>{data.title}</h1>
+          <Image src={data.author_img} width={60} height={60} alt='' className='mx-auto mt-6 border border-white rounded-full'/>
+          <p className=' mt-1 pb-2 text-lg max-w-185 mx-auto'>{data.author}</p>
+      </div>
     </div>
+    <div className=' mx-5 max-w-175 md:mx-auto mt-[-100] mb-100'>
+      <Image src={data.image} alt='' width={1280} height={720}/>
+    </div>
+    </>
+    :<></>
   )
 }
 
